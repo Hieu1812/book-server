c9aae1a76209ea7fd673577f1fc3b6d9bf8c6f2b
Hieu <hieupl2004@gmail.com>
2025-03-26 17:07:44 +0700

    update

--- routes/order.js
+++ routes/order.js
@@ -6,6 +6,6 @@ const ordercon = require('../controller/ordercon.js');
 router.get('/getOrderByID/:id_order', ordercon.getOrderByID);  // lấy đơn hàng theo mã đơn hàng
 router.get('/getOrders', ordercon.getOrders); // lấy tất cả đơn hàng
 router.post('/addOrderAndOrderDetail', ordercon.addOrderAndOrderDetail);
-
 router.post('/:id_order/status', ordercon.updateOrderStatus);
+router.post('/:id_order/cancel', ordercon.getOrderByAccountID);
 module.exports = router;
