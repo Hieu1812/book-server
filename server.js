d9ffdc7c46bbe2392b18a78a49caee63c6712b7c
Hieu <hieupl2004@gmail.com>
2025-05-23 23:50:44 +0700

    test pipe

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const config = {
 
 app.get('/', (req, res) => {
     res.send(
-      `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
+      `<body style='background-color:#283E5B'><h1 style='color: red;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
     );
   });
 
